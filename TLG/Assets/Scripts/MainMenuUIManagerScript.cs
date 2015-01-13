8ba134c86dbd4c4c7b640b1615994c94e270ec54
joemo65 <joemo65@gmail.com>
2015-01-12 21:45:01 -0800

    Added Initial Project

---
+++ TLG/Assets/Scripts/MainMenuUIManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuUIManagerScript : MonoBehaviour {
+	public void Play()
+	{
+		Application.LoadLevel ("CharacterMenuScene");
+	}
+
+	public void Leaderboards()
+	{
+		Application.LoadLevel ("LeaderboardsScene");
+	}
+}
---
+++ TLG/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour {
+
+	public float spawnTime = 5f;		// The amount of time between each spawn.
+	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public GameObject[] enemies;		// Array of enemies.
+
+	// Use this for initialization
+	void Start () {
+		//calls the spawn function after the 
+		InvokeRepeating ("Spawn", spawnDelay, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Spawn()
+	{
+		int numberOfEnemies = 10;
+
+		//create enemy 1
+		//Instantiate (enemies [numberOfEnemies], Transform.position, Transform.rotation);
+
+	}
+}
