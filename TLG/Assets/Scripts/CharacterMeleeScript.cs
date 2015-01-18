1ef5f59c50aeea020ef800790a9e6a03089f8243
joemo65 <joemo65@gmail.com>
2015-01-18 09:55:47 -0800

    created melee swing animation for main character.

--- TLG/Assets/Scripts/CharacterMeleeScript.cs
+++ TLG/Assets/Scripts/CharacterMeleeScript.cs
@@ -6,15 +6,25 @@ public class CharacterMeleeScript : MonoBehaviour {
     private float damage;
     private float coolDown;
     private float swingTime;
+    private Animator meleeAnimation;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+    void Awake()
+    {
+        meleeAnimation = transform.root.gameObject.GetComponent<Animator>();
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+        //if they press the melee button
+        if (Input.GetButtonDown("Fire1"))
+        {
+            meleeAnimation.SetTrigger("Melee");
+        }
 	}
 
 
